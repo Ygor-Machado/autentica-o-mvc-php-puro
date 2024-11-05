60bac79fed288d4d3564b658096d3750df4ebccb
Ygao-Underground <ygaounderground011@gmail.com>
2024-11-05 02:11:31 -0300

    fix: corrige erro de requisição para o método logout

--- src/Views/dashboard.php
+++ src/Views/dashboard.php
@@ -4,7 +4,9 @@
     <h1 class="display-4">Bem vindo ao dashboard da area administrativa!</h1>
     <p class="lead">Apenas pessoas autenticadas.</p>
     <hr class="my-4">
-    <p>
-        <a href="/logout" class="btn btn-danger btn-lg" role="button">Logout</a>
-    </p>
+
+    <form action="/logout"  method="POST">
+        <button class="btn btn-danger btn-lg" type="submit">Logout</button>
+    </form>
+
 </div>
